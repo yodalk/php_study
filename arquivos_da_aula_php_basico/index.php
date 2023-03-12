11238e26984115cf07ab73c18931b3f3a2ef9a18
Yodalk Developer <46011897+yodalk@users.noreply.github.com>
2023-03-11 22:31:05 -0800

    Adicionando arquivos do curs

---
+++ arquivos_da_aula_php_basico/index.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <?php
+        echo 'Utilizando a tag padrão';
+    ?>
+    <br/>
+    <?= 'Utilizando a tag impressão' ?>
+</body>
+
+</html>
